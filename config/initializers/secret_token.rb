308af72afe984cf4d43528fabdc8be42eda47716
yak <yak@oschadnybank.donetsk.ua>
2013-02-18 16:20:30 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f7606228ec4793ae9e70731caeb4dda61f0cf72107cf4163b1552cf40aca8afa214a8164f61d66e005d3d24ee22cfe3311516a82adc3e9f968d6db98c0d5541f'
